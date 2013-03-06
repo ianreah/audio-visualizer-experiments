b60cd17fd2519ae4e4b46c15f52cb776b511a94b
Ian Reah <ian.reah@gmail.com>
2013-03-06 09:02:25 +0000

    Introduce requireJS and jsFrames

--- scripts/frequency-bar-chart.js
+++ scripts/frequency-bar-chart.js
@@ -1,4 +1,4 @@
-$(function () {
+define(['jquery', 'jsFrames.min'], function ($, jsFrames) {
     // Future-proofing...
     var context;
     if (typeof AudioContext !== "undefined") {
@@ -10,34 +10,6 @@ $(function () {
         $(".showIfNoApi").show();
         return;
     }
-
-    // Overkill - if we've got Web Audio API, surely we've got requestAnimationFrame. Surely?...
-    // requestAnimationFrame polyfill by Erik Möller
-    // fixes from Paul Irish and Tino Zijdel
-    // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-    // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
-                                    || window[vendors[x] + 'CancelRequestAnimationFrame'];
-    }
-
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function (callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function () { callback(currTime + timeToCall); },
-                timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function (id) {
-            clearTimeout(id);
-        };
 
     // Create the analyser
     var analyser = context.createAnalyser();
@@ -52,16 +24,14 @@ $(function () {
     }
     var bars = $("#visualisation > div");
 
-    // Get the frequency data and update the visualisation
-    function update() {
-        requestAnimationFrame(update);
-
-        analyser.getByteFrequencyData(frequencyData);
+    jsFrames.registerAnimation(function () {
+    	// Get the frequency data and update the visualisation
+    	analyser.getByteFrequencyData(frequencyData);
 
         bars.each(function (index, bar) {
             bar.style.height = frequencyData[index] + 'px';
         });
-    };
+    });
 
     // Hook up the audio routing...
     // player -> analyser -> speakers
@@ -73,5 +43,5 @@ $(function () {
 	});
 
     // Kick it off...
-    update();
+    jsFrames.start();
 });
