1bdafefd39840a55caab4567cc2bf0e4bd970d21
Ian Reah <ian.reah@gmail.com>
2013-03-06 12:16:21 +0000

    Update 2d-equation to match updates in bar chart example

--- scripts/2d-equation.js
+++ scripts/2d-equation.js
@@ -1,5 +1,5 @@
-$(function () {
-    // Future-proofing...
+require(['jquery', 'jsFrames.min'], function ($, jsFrames) {
+	// Future-proofing...
     var context;
     if (typeof AudioContext !== "undefined") {
         context = new AudioContext();
@@ -10,34 +10,6 @@ $(function () {
         $(".showIfNoApi").show();
         return;
     }
-
-    // Overkill - if we've got Web Audio API, surely we've got requestAnimationFrame. Surely?...
-    // requestAnimationFrame polyfill by Erik Möller
-    // fixes from Paul Irish and Tino Zijdel
-    // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-    // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
-                                    || window[vendors[x] + 'CancelRequestAnimationFrame'];
-    }
-
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function (callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function () { callback(currTime + timeToCall); },
-                timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function (id) {
-            clearTimeout(id);
-        };
 
     // Create the analyser
     var analyser = context.createAnalyser();
@@ -52,10 +24,9 @@ $(function () {
     var imageData = canvasContext.createImageData(1, 1);
     var pixelData = imageData.data;
 
-    // Get the frequency data and update the visualisation
-    function update() {
-        requestAnimationFrame(update);
-        analyser.getByteFrequencyData(frequencyData);
+    jsFrames.registerAnimation(function () {
+    	// Get the frequency data and update the visualisation
+    	analyser.getByteFrequencyData(frequencyData);
 		
 		var averagedData = new Array(0,0,0,0);
 		for(var index = 0; index < analyser.frequencyBinCount; index++) {
@@ -102,8 +73,13 @@ $(function () {
 		// Duplicate the canvas
 		duplicateContext.clearRect(0, 0, 255, 255);
 		duplicateContext.drawImage(canvas, 0, 0);
-    };
+    });
 
+    var theFpsDisplay = $('#fps');
+    jsFrames.onFpsUpdate(function (fps) {
+    	theFpsDisplay.html(fps);
+    });
+	
     // Hook up the audio routing...
     // player -> analyser -> speakers
 	// (Do this after the player is ready to play - https://code.google.com/p/chromium/issues/detail?id=112368#c4)
@@ -114,5 +90,5 @@ $(function () {
 	});
 	
     // Kick it off...
-    update();
+	jsFrames.start();
 });
